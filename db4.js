ba977dbd59bb3c1b4e14251149dc1add51936c69
Ammar-175 <raja.ammar1005@gmail.com>
2024-10-23 00:26:02 +0500

    file is deleted

--- db4.js
+++
@@ -1,8 +0,0 @@
-show dbs 
-use ammar
-show collections
-
-db.items.find()
-db.items.updateOne({name : "Vivo"} , {$set : {price : 5}})
-db.items.find()
-db.items.updateMany({price : 200} , {$set : {name : "ok"}})
